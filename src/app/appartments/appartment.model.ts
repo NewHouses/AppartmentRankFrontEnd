5798cc2ed14663a3606c90723a34c153f9c5d9d1
NewHouses <sergio.simons.c@gmail.com>
2023-02-14 19:02:44 +0100

    adding appartment model and filling appartment list

---
+++ src/app/appartments/appartment.model.ts
@@ -0,0 +1,13 @@
+export class Appartment {
+  public name: string;
+  public link: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, link: string, description: string, imagePath: string) {
+    this.name = name;
+    this.link = link;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
