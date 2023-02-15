9d04a742063391efbcffb9f6f71908ad9f4c7578
NewHouses <sergio.simons.c@gmail.com>
2023-02-15 15:30:03 +0100

    Moving apartment item logic to a the new component

--- src/app/apartments/apartment-list/apartment-item/apartment-item.component.ts
+++ src/app/apartments/apartment-list/apartment-item/apartment-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Apartment } from '../../apartment.model';
 
 @Component({
   selector: 'app-apartment-item',
   templateUrl: './apartment-item.component.html',
   styleUrls: ['./apartment-item.component.css']
 })
-export class ApartmentItemComponent {
+export class ApartmentItemComponent implements OnInit {
+  @Input() apartment!: Apartment;
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
