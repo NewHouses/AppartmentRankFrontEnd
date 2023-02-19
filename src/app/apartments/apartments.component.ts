32810d9d90e6b716a238b597d8f08957f2e28c00
NewHouses <sergio.simons.c@gmail.com>
2023-02-19 21:19:27 +0100

    Adding ApartmentService as Service in charge of get and provide data about the available Apartments

--- src/app/apartments/apartments.component.ts
+++ src/app/apartments/apartments.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Apartment } from './apartment.model';
+import { ApartmentService } from './apartment.service';
 
 @Component({
   selector: 'app-apartments',
   templateUrl: './apartments.component.html',
-  styleUrls: ['./apartments.component.css']
+  styleUrls: ['./apartments.component.css'],
+  providers: [ApartmentService]
 })
 export class ApartmentsComponent {
   selectedApartment!: Apartment;
