4e62ac46bda2533a7433aec07fdeece31933164f
NewHouses <sergio.simons.c@gmail.com>
2023-02-19 21:51:22 +0100

    Adding apartmentAttributes to Apartment Model

--- src/app/apartments/apartment.model.ts
+++ src/app/apartments/apartment.model.ts
@@ -1,13 +1,17 @@
+import { ApartmentAttribute } from "../shared/apartmentAttribute.model";
+
 export class Apartment {
   public name: string;
   public link: string;
   public description: string;
   public imagePath: string;
+  public apartmentAttributes: ApartmentAttribute[];
 
-  constructor(name: string, link: string, description: string, imagePath: string) {
+  constructor(name: string, link: string, description: string, imagePath: string, apartmentAttributes: ApartmentAttribute[]) {
     this.name = name;
     this.link = link;
     this.description = description;
     this.imagePath = imagePath;
+    this.apartmentAttributes = apartmentAttributes;
   }
 }
--- src/app/apartments/apartment.service.ts
+++ src/app/apartments/apartment.service.ts
@@ -1,9 +1,19 @@
 import { EventEmitter } from "@angular/core";
+import { ApartmentAttribute } from "../shared/apartmentAttribute.model";
 import { Apartment } from "./apartment.model";
 
 export class ApartmentService {
   private apartments: Apartment[] = [
-    new Apartment('Alquiler de Piso en As Travesas - Balaídos', 'https://www.idealista.com/inmueble/100121936/', 'ALQUILER PISO 3 HAB. GARAJE Y TRASTERO ZONA FLORIDA\n¿Estás buscando amplitud, luminosidad, situación y comodidad?\n¡Lo tenemos!\nPrecioso piso de 100m2, recién reformado y en el que la luz es su principal atractivo.\nSe compone de: 3 dormitorios, 1 despacho con chimenea, 2 baños, amplio salón - comedor con acceso directo a la terraza con vistas al Lagares, cocina totalmente equipada y amueblada y zona de lavandería independiente.\n" Disfrutar de una cervecita al aire libre y sí además tiene vistas verdes.. . no tiene precio"\nSituado en una zona con todos los servicios a mano, para hacerte el día a día más fácil.\nAdemás incluye plaza de garaje doble y trastero.\nSe alquila totalmente amueblado.\nNo admite mascotas.\nLlámanos, no lo dejes escapar ?', 'https://img3.idealista.com/blur/WEB_DETAIL_TOP-L-L/0/id.pro.es.image.master/7e/34/08/1069924079.jpg')
+    new Apartment('Alquiler de Piso en As Travesas - Balaídos',
+      'https://www.idealista.com/inmueble/100121936/',
+      'ALQUILER PISO 3 HAB. GARAJE Y TRASTERO ZONA FLORIDA\n¿Estás buscando amplitud, luminosidad, situación y comodidad?\n¡Lo tenemos!\nPrecioso piso de 100m2, recién reformado y en el que la luz es su principal atractivo.\nSe compone de: 3 dormitorios, 1 despacho con chimenea, 2 baños, amplio salón - comedor con acceso directo a la terraza con vistas al Lagares, cocina totalmente equipada y amueblada y zona de lavandería independiente.\n" Disfrutar de una cervecita al aire libre y sí además tiene vistas verdes.. . no tiene precio"\nSituado en una zona con todos los servicios a mano, para hacerte el día a día más fácil.\nAdemás incluye plaza de garaje doble y trastero.\nSe alquila totalmente amueblado.\nNo admite mascotas.\nLlámanos, no lo dejes escapar ?',
+      'https://img3.idealista.com/blur/WEB_DETAIL_TOP-L-L/0/id.pro.es.image.master/7e/34/08/1069924079.jpg',
+      [
+        new ApartmentAttribute('Terraza', 2),
+        new ApartmentAttribute('Lavalouzas', 1),
+        new ApartmentAttribute('Lavadoura', 2)
+      ]
+    )
   ];
 
   apartmentSelected = new EventEmitter<Apartment>();
