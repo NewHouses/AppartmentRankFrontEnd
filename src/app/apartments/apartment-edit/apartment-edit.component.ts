b4979ba2823baf3fd817217a097d0a76a665a83d
NewHouses <sergio.simons.c@gmail.com>
2023-02-22 23:56:22 +0100

    syncronizing the create/edit apartment html with the form

--- src/app/apartments/apartment-edit/apartment-edit.component.ts
+++ src/app/apartments/apartment-edit/apartment-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ApartmentService } from '../apartment.service';
 
 @Component({
   selector: 'app-apartment-edit',
@@ -9,8 +11,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ApartmentEditComponent implements OnInit {
   apartmentId: number;
   editMode = false;
+  apartmentForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private apartmentService: ApartmentService) {
 
   }
 
@@ -19,7 +22,31 @@ export class ApartmentEditComponent implements OnInit {
       (params: Params) => {
         this.apartmentId = +params['apartmentId'];
         this.editMode = params['apartmentId'] != null;
+        this.initForm();
       }
     );
+  }
+
+  onSubmit() {
+    console.log(this.apartmentForm);
+  }
+
+  private initForm() {
+    let apartmentName = '';
+    let apartmentImagePath = '';
+    let apartmentDescription = '';
+
+    if (this.editMode) {
+      const apartment = this.apartmentService.getApartment(this.apartmentId);
+      apartmentName = apartment.name;
+      apartmentImagePath = apartment.imagePath;
+      apartmentDescription = apartment.description;
+    }
+
+    this.apartmentForm = new FormGroup({
+      'name': new FormControl(apartmentName),
+      'imagePath': new FormControl(apartmentImagePath),
+      'description': new FormControl(apartmentDescription)
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -36,7 +36,8 @@ import { ApartmentEditComponent } from './apartments/apartment-edit/apartment-ed
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [PreferenceService],
   bootstrap: [AppComponent]
