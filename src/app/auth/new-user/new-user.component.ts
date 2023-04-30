ce2d7adfe08af4cf2bd32bb142f6c8edc78e596f
NewHouses <sergio.simons.c@gmail.com>
2023-04-30 21:12:23 +0200

    setting signup

--- src/app/auth/new-user/new-user.component.ts
+++ src/app/auth/new-user/new-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-new-user',
@@ -9,15 +10,28 @@ import { NgForm } from '@angular/forms';
 export class NewUserComponent {
   areNotEqualPasswords: boolean;
 
-  onSubmit(form: NgForm) {
-    const value = form.value;
+  constructor(private authService: AuthService) {
 
-    if (value.password != value.repeatedPassword) {
+  }
+
+  onSubmit(form: NgForm) {
+    const username = form.value.username;
+    const password = form.value.password;
+    const repeatedPassword = form.value.repeatedPassword;
+
+    if (password != repeatedPassword) {
       this.areNotEqualPasswords = true;
+      return;
     }
-    else {
-      this.areNotEqualPasswords = false;
-      form.reset();
-    }
+
+    this.areNotEqualPasswords = false;
+
+    this.authService.signup(username, password).subscribe(resData => {
+      console.log(resData);
+    }, error => {
+      console.log(error);
+    });
+
+    form.reset();
   }
 }
