fd4d625e86f9886df293993bc69ef5a226f5f1fc
NewHouses <sergio.simons.c@gmail.com>
2023-05-02 16:11:49 +0200

    Creating user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,37 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { tap } from "rxjs/operators";
+import { User } from "./user.model";
 
-export class AuthResponseData {
-  public message: string;
+interface AuthResponseData {
+  Message: string;
+  username: string;
+  token: string;
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) { }
 
   signup(username: string, password: string) {
     return this.http.post<AuthResponseData>('https://localhost:7106/Authentication/signUp',
       {
-        username: username,
-        password: password,
+        Username: username,
+        Password: password
       }
-    );
+    ).pipe(tap(resData => {
+      this.handleAuthentication(resData.username, resData.token)
+    }));
+  }
+
+  private handleAuthentication(username: string, token: string) {
+    if (token != null) {
+      const user = new User(username, token)
+      this.user.next(user);
+      console.log(this.user);
+    }
   }
 }
